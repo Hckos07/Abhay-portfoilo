2ba5a9147109622d1ae9b5143ea26c0b79f88e59
Abhay Pal <abhaypal1298@gmail.com>
2024-12-10 12:06:27 +0530

    firstcommit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <header className='fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0'>
+      <div className='max-w-screen-sm w-full mx-auto px-4 '>
+
+      </div>
+    </header>
+  )
+}
+
+export default header
